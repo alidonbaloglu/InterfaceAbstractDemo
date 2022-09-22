e0c8533b7278ee47af0da5e01467d6471282c1d5
Ali Donbaloğlu <72136581+alidonbaloglu@users.noreply.github.com>
2022-09-22 08:56:51 +0300

    Add project files.

---
+++ InterfaceAbstractDemo/Concrete/StarbucksCustomerManager.cs
@@ -0,0 +1,26 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstractDemo.Concrete
+{
+    public  class StarbucksCustomerManager:BaseCustomerManager,ICustomerCheckService
+    {
+        public override void Save(Customer customer)
+        {
+            CheckIfRealPerson(customer);
+            base.Save(customer);
+        }
+
+        public void CheckIfRealPerson(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ InterfaceAbstractDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using System;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager =  new NeroCustomerManager();
+            customerManager.Save(new Entities.Customer { DateOfBirth = new DateTime(1985, 1, 6), FirstName = "Engin", LastName = "Demiroğ", NationalityId = "182465684" });
+
+        
+        }
+    }
+   
+        
+}
